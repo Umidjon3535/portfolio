89835e6abdeae9700385bffc8153c95518944586
Umidjon <nuriddinu79@gmail.com>
2026-09-22 17:53:09 +0500

    Add media images to repo and fix media serving for Vercel production

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
+from django.urls import re_path
 
 from config import settings
 
@@ -9,6 +11,10 @@ urlpatterns = [
     path('', include("portfolio.urls")),
 ]
 
+# Serve media files in both DEBUG and production (needed for Vercel)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
